265399b268ea596eb132bca9f79054845fc6bb52
Andre Diaz <andrediaz@MacBook-Pro-de-Andre.local>
2021-01-04 03:55:09 -0500

    first commit

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
